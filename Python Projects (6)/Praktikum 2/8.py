81b1089d6ed9f3dad99eae59ca004c2916e903fb
NizarArhaNaufal <72216735+NizarArhaNaufal@users.noreply.github.com>
2020-11-05 00:56:35 +0700

    Add files via upload

---
+++ Python Projects (6)/Praktikum 2/8.py
@@ -0,0 +1,19 @@
+def luasSegitiga(a,t):
+    luas = a * t / 2
+    return luas
+
+alas = 10
+tinggi = 20
+print("Luas segitiga dengan alas", alas,
+      "dan tinggi", tinggi,
+      "adalah", luasSegitiga(alas,tinggi))
+luasSegitiga1 = luasSegitiga(alas, tinggi)
+
+alas = 15
+tinggi = 45
+print("Luas segitiga dengan alas", alas,
+      "dan tinggi", tinggi,
+      "adalah", luasSegitiga(alas,tinggi))
+luasSegitiga2 = luasSegitiga(alas, tinggi)
+
+print("Jumlah luas seluruh segitiga adalah", luasSegitiga1 + luasSegitiga2)
